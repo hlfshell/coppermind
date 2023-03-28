479e38c862e9d8c743ca05d92f57537d2e3386d0
keith <kchester@gmail.com>
2023-03-28 13:17:53 -0700

    feature: Updates on summarization + prompts. knoledge prompts WIP

--- internal/memory/summary.go
+++ internal/memory/summary.go
@@ -8,13 +8,13 @@ import (
 )
 
 type Summary struct {
-	ID           string
-	Agent        string
-	Conversation string
-	Keywords     []string
-	Brief        string
-	User         string
-	CreatedAt    time.Time
+	ID           string    `json:"id,omitempty" db:"id"`
+	Agent        string    `json:"agent,omitempty" db:"agent"`
+	Conversation string    `json:"conversation,omitempty" db:"conversation"`
+	Keywords     []string  `json:"keywords,omitempty" db:"keywords"`
+	Summary      string    `json:"summary,omitempty" db:"summary"`
+	User         string    `json:"user,omitempty" db:"user"`
+	CreatedAt    time.Time `json:"created_at,omitempty" db:"created_at"`
 }
 
 func (summary *Summary) KeywordsToString() string {
@@ -29,6 +29,6 @@ func (summary *Summary) String() string {
 	return stringFormatter.Format(
 		"{0} | {1}",
 		summary.KeywordsToString(),
-		summary.Brief,
+		summary.Summary,
 	)
 }
