5b520fe8bcd72e22c2990fe6b038b27c61f94ca7
keith <kchester@gmail.com>
2023-04-07 13:53:22 -0700

    test: expanded store test coverage

--- internal/store/sqlite/sqlite_test.go
+++ internal/store/sqlite/sqlite_test.go
@@ -25,14 +25,16 @@ func TestMigrate(t *testing.T) {
 
 func TestSqlite(t *testing.T) {
 	tests := map[string]func(*testing.T, store.Store){
-		"GetLatestConversation":          storeTest.GetLatestConversation,
-		"GetConversations":               storeTest.GetConversation,
-		"SaveMessage":                    storeTest.SaveMessage,
-		"GetConversationsToSummarize":    storeTest.GetConversationsToSummarize,
-		"GetSummaryByConversation":       storeTest.GetSummaryByConversation,
-		"SaveSummary":                    storeTest.SaveSummary,
-		"GetSummariesByAgentAndUser":     storeTest.GetSummariesByAgentAndUser,
-		"ExcludeConversationFromSummary": storeTest.ExcludeConversationFromSummary,
+		"GetLatestConversation":               storeTest.GetLatestConversation,
+		"GetConversations":                    storeTest.GetConversation,
+		"SaveMessage":                         storeTest.SaveMessage,
+		"GetConversationsToSummarize":         storeTest.GetConversationsToSummarize,
+		"GetSummaryByConversation":            storeTest.GetSummaryByConversation,
+		"SaveSummary":                         storeTest.SaveSummary,
+		"GetSummariesByAgentAndUser":          storeTest.GetSummariesByAgentAndUser,
+		"ExcludeConversationFromSummary":      storeTest.ExcludeConversationFromSummary,
+		"ExpireKnowledge":                     storeTest.ExpireKnowledge,
+		"SetConversationAsKnowledgeExtracted": storeTest.SetConversationAsKnowledgeExtracted,
 	}
 
 	for name, test := range tests {
